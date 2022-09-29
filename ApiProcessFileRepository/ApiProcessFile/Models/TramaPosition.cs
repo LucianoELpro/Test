d36db35fbdb6f6c883d85f6f247e1c90c36b1fdc
LUIS ANGEL VARGAS HILARIO <luis.vargash@usil.pe>
2022-09-29 01:06:20 -0500

    tramaposition local

--- ApiProcessFileRepository/ApiProcessFile/Models/TramaPosition.cs
+++ ApiProcessFileRepository/ApiProcessFile/Models/TramaPosition.cs
@@ -8,15 +8,13 @@ namespace ApiProcessFile.Models
 {
     public class TramaPosition
     {
-<<<<<<< HEAD
-=======
+
         public string TypeValue { get; set; }
 
         public string Value { get; set; }
-        public string Value2 { get; set; }
-        public string Vd3{ get; set; }
+        
         public string Value4{ get; set; }
->>>>>>> local2
+
 
        
 
